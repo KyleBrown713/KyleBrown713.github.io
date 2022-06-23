32de7e5914690db6d0526f5115c6125ebc945127
Kyle Brown <kyle.brown@cleverbridge.com>
2022-06-23 15:50:01 -0500

    header styling

---
+++ js/header.js
@@ -0,0 +1,14 @@
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {stickyHeader()};
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyHeader() {
+    var header = document.getElementById("site-header");
+    var sticky = 100;
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
