289708157647a2c7980e0c7dd1ce9540b30bb568
Kyle Brown <kyle.brown@cleverbridge.com>
2022-06-24 15:11:05 -0500

    post image and home

--- js/override.js
+++ js/override.js
@@ -5,7 +5,7 @@ window.onscroll = function() {stickyHeader()};
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyHeader() {
     var header = document.getElementById("site-header");
-    var sticky = 100;
+    var sticky = 0;
     if (window.pageYOffset > sticky) {
         header.classList.add("sticky");
     } else {
